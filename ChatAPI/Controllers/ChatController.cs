c83e8db1b026952c1e0ec115f088b5f78e45f390
sohelafzalshajol <sohelafzalshajol@gmail.com>
2024-04-14 00:17:14 +0600

    commit after single chat services using SignalR

---
+++ ChatAPI/Controllers/ChatController.cs
@@ -0,0 +1,31 @@
+﻿using ChatAPI.Models;
+using ChatAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly ChatService _chatService;
+        public ChatController(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        [HttpPost("register-user")]
+        public IActionResult RegisterUser(UserDTO model) 
+        {
+            if (_chatService.AddUserToList(model.UserEmail))
+            {
+                return NoContent();
+            }
+            else
+            {
+                return BadRequest("This user is already created!");
+            }
+        }
+    }
+}
