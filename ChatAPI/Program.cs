4cb09ff16f181a2cad8292edd8a889e16156f204
sohelafzalshajol <sohelafzalshajol@gmail.com>
2024-04-14 00:28:05 +0600

    update commit

--- ChatAPI/Program.cs
+++ ChatAPI/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
